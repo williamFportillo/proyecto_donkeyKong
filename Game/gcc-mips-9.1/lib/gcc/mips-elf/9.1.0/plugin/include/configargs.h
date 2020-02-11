a801b22efed00e123ff10d265b2553d8aac70c77
williamFportillo <portillowilliam18@gmail.com>
2020-02-11 17:36:58 -0600

    Borrando Carpetas Innecesarias

--- Game/gcc-mips-9.1/lib/gcc/mips-elf/9.1.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc-9.1.0/configure --prefix=/opt/gcc-mips-9.1 --target=mips-elf --enable-languages=c,c++,lto --with-arch-32=mips1 --enable-lto --disable-nls --disable-shared --disable-libstdc___v3 --disable-decimal-float --disable-threads --disable-libmudflap --disable-libssp --disable-libgomp --disable-libquadmath --disable-multilib";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "arch_32", "mips1" } };
